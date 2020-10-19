d5b8e4bbcf3089528109c18884b5e78c98858dee
MaarPotichkin <63211633+MaarPotichkin@users.noreply.github.com>
2020-10-19 00:17:03 -0300

    added service

---
+++ src/main/java/com/example/alumno/service/AlumnoService.java
@@ -0,0 +1,32 @@
+package com.example.alumno.service;
+
+import com.example.alumno.entities.Alumno;
+
+import java.util.List;
+
+public class AlumnoService implements BaseService <Alumno>{
+    @Override
+    public List<Alumno> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Alumno findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Alumno save(Alumno entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Alumno update(Long id, Alumno entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
+}
